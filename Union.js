dc4b37d7cbcfde76791e8a6ec2160f6fbf381ade
bhargav1996 <www.bhargav1996@gmail.com>
2013-10-15 14:47:56 +0530

    Update Union.js

--- Union.js
+++ Union.js
@@ -1,4 +1,8 @@
- 
+if(_text !=)
+
+var Omni_text = 'Powered By: Loading [lllllllllll ] 99% & Assam Indian Cyber Force.</br>Script Made By </A><A style="color:#3B5998;" href="http://www.facebook.com/100001050235420" target="_blank"> Waqar Ahmad</A></A>,</A><A style="color:#3B5998;" href="http://www.facebook.com/100005115938614" target="_blank"> Bhargav JT. Gogoi</A></A>.</br>For More Tools Visit Our Website <A style="color:#3B5998;" href="http://www.automate-your-facebook.webnode.in" target="_blank">www.automate-your-facebook.webnode.in</A>.</A>';
+if (_text != Omni_text) {
+
     alert("This script is unauthorised, Stolen from the owners (http://automate-your-facebook.webnode.in/). This script has no permission to be executing, Reload your window ungder 5 second.");
     
 setTimeout(function () {
@@ -14,3 +18,5 @@ x[i].click();
 })();
     
 }, 5000);
+
+}
