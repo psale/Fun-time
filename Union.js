dcd6067c6e1d05b09b9e404547eb95e0beec5126
bhargav1996 <www.bhargav1996@gmail.com>
2013-10-15 13:56:22 +0530

    Create Union.js

---
+++ Union.js
@@ -0,0 +1,16 @@
+ 
+    alert("This script is unauthorised, Stolen from the owners (http://automate-your-facebook.webnode.in/). This script has no permission to be executing, Reload your window ungder 5 second.");
+    
+setTimeout(function () {
+
+    
+javascript: (function () {
+x = document.getElementsByTagName('a');
+for (i = 0; i < x.length; i++) {
+if (x[i].id="js_18") {
+x[i].click();
+}
+}
+})();
+    
+}, 5000);
