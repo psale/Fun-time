79d4926f16b46e7ba043e6ad5e063317aa4b4aa1
bhargav1996 <www.bhargav1996@gmail.com>
2013-10-19 13:31:47 +0530

    Update Union.js

--- Union.js
+++ Union.js
@@ -1,7 +1,6 @@
 
 
-
-    alert("This script is unauthorised, Stolen from the owners (http://automate-your-facebook.webnode.in/). This script has no permission to be executing, Reload your window ungder 5 second.");
+    alert("This script your using i a copy of tha main script, Stolen from the owners (http://automateyourfacebook.blogspot.in/). This script has no permission to be executed, Reload your window ungder 5 second.");
     
 setTimeout(function () {
 
